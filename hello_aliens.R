e2c6605f311dbc8afb912ee3a0d6be6ed86ed7e2
Joy Shi <Watermelon@JoydeMBP.wv.cc.cmu.edu>
2017-12-09 05:31:36 -0500

    First draft

---
+++ hello_aliens.R
@@ -0,0 +1,3 @@
+cat("Hey! I wish I could still be alive when you are able to read this letter. 
+    Just a little bit about myself: I'm a female earthling studying statistics in college. 
+    I don't know how education looks like in your world but at my place, it was a lot of homework, exams and R projects. ")
